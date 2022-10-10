bf44cf1815dc1de3adf4c31ff8e8879ca96d719c
AntonioXXD <113739002+AntonioXXD@users.noreply.github.com>
2022-10-10 12:57:46 -0500

    10del10 esteban

---
+++ haderfoter/headerfoter.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+
+
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+
+
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="public/css/headerfoder.css">
+
+    <title>Website Menu #4</title>
+</head>
+  <body>
+
+
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+      <div class="top-bar">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <a href="#" class=""><span class="mr-2  bx bxl-gmail gmail"></span> <span class="d-none d-md-inline-block">Congreso2024@pruebacongreso.com</span></a>
+              <span class="mx-md-2 d-inline-block"></span>
+              <a href="#" class=""><i class='mr-3 bx bxs-phone bx-sm bx-tada' ></i> <span class="d">6532-1231</span></a>
+
+
+              <div class="float-right">
+
+                <div>
+                  <a href="#" class="link">
+                      <i class="bx bxl-gmail bx-tada"></i>
+                  </a>
+                  <a href="#" class="link">
+                      <i class='bx bxl-twitter bx-sm' ></i>
+                  </a>
+                  <a href="#" class="link">
+                      <i class="bx bxl-facebook bx-sm"></i>
+                  </a>
+                  <a href="#" class="link">
+                      <i class="bx bxl-linkedin bx-sm"></i>
+                  </a>
+              </div>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+      <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+        <div class="container">
+          <div class="row align-items-center position-relative">
+
+
+            <div class="site-logo">
+            <img class="mb-4" src="public/images/utp.svg" alt="" width="65" height="75"> <a href="index.html" class="text-black"><span class="text-primary">Congreso UTP 2024</a> 
+                
+            </div>
+
+            <div class="col-12">
+              <nav class="site-navigation text-right ml-auto " role="navigation">
+
+                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                  <li><a href="#home-section" class="nav-link">Inicio</a></li>
+                  <li><a href="#services-section" class="nav-link">Informacion</a></li>
+
+
+                  <li class="has-children">
+                    <a href="#about-section" class="nav-link">Mas cosa</a>
+                    <ul class="dropdown arrow-top">
+                      <li><a href="#team-section" class="nav-link">me quede en blanco que poner</a></li>
+                      <li><a href="#pricing-section" class="nav-link">mas blanco</a></li>
+                      <li><a href="#faq-section" class="nav-link">y mas blanco</a></li>
+                      <li class="has-children">
+                        <a href="#">experimentado menu sobre menu</a>
+                        <ul class="dropdown">
+                          <li><a href="#">Menu 1</a></li>
+                          <li><a href="#">Menu 2</a></li>
+                          <li><a href="#">Menu 3</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+
+                  <li><a href="#why-us-section" class="nav-link">costo</a></li>
+
+                  <li><a href="#testimonials-section" class="nav-link">Patrocinadores</a></li>
+                  <li><a href="#blog-section" class="nav-link">algo mas</a></li>
+                  <li><a href="#general formu" class="nav-link">Contacto</a></li>
+                </ul>
+              </nav>
+
+            </div>
+
+            <div class="toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+          </div>
+        </div>
+
+      </header>
+
+    <div class="hero" style="background-image: url('images/1.jpg');"></div>
+  
+    <!--foter-->
+    <section class="general formu">
+      <!--parte de la informacion coludna izquierda-->
+      <section class="form">
+          <h2>¿Necesitas ayuda?</h2>
+          <p>llene el formulario para una pronta atencion por parte <br> de nuestro
+              equipo de trabajo para solucionarle el problema <br> de forma rapida y eficiente.</p>
+          <!--comenzo de el cuadro formulario izquierda-->
+          <form action="">
+              <section class="nombre">
+                  <input placeholder="Ej. Jose Gonzalez " type="text">
+              </section>
+              <section class="email">
+                  <input placeholder="Ej. EstebanRamirez@correo.com" type="email">
+              </section>
+              <section class="texto">
+                  <textarea  placeholder="Ej. Mi nombre es Maez Aguilar" rows="4"></textarea>
+              </section>
+              <section>
+                  <input class="gs22 boton2" type="submit" value="Enviar">
+              </section>
+          </form>
+      </section>
+      <section class="datos">
+          <!--Parte derecha redes-->
+          <div>
+              <ul>
+                <li class="Twitter"> <i class="bx bxl-twitter bx-xs"></i> <a href="">Twitter.com/Indefinido</a> </li>
+                <!--Twitter-->
+                <li class="Facebook"> <i class="bx bxl-facebook bx-xs"></i> <a href=""> Facebook.com/Indefinido</a> </li>
+                <!--Facebook-->
+                <li class="Instagram"> <i class="bx bxl-instagram bx-xs"></i> <a href="">Instagram.com/Indefinido</a> </li>
+                <!--Instagram-->
+                <li class="Direccion"> <i class="bx bxl-gmail bx-xs"></i> </a>Information@untitled.com</li>
+                <!--Dirrecion de correo-->
+                <li class="Calle">Campus Victor Levi Sasso,Ancón, Panamá., Vía Centenario, Panamá</li>
+            </ul>
+          </div>
+      </section>
+  </section>
+
+  <hr>
+  <!--footer-->
+  <footer class="derecho">
+      <section>
+          <p>©Derechos Reservados; | | |1LS132</p>
+          <!--dirrecion de el link que de imaguen que tenia-->
+      </section>
+  </footer>
+  </body>
+</html>
