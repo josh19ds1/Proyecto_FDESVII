2f8b04507ae685075c578c9c4794a896c4e3bd0c
Arundel97 <Arundel97@hotmail.com>
2022-10-11 12:46:47 -0500

    Update controles.php
    actualizacion

--- controller/controles.php
+++ controller/controles.php
@@ -1,2 +1,25 @@
-<?php ?>
-<h2>Observacion2</h2>
+<?php
+
+class controlador{
+
+
+    public function Index(){
+
+        require("view/PaginaInicio.php");
+    }
+
+
+    public function Registrar(){
+
+            require("view/Registro.php");
+
+    }
+
+    public function Administracion(){
+
+        require("view/registropersonal.php");
+
+    }   
+
+
+}
