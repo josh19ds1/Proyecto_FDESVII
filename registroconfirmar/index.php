b8902f5da7bbe63bd859409638647982e08be0ba
AntonioXXD <113739002+AntonioXXD@users.noreply.github.com>
2022-10-10 23:29:49 -0500

    cambio 11pm 10del10

---
+++ registroconfirmar/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="public/css/confirmarregistro.css">
+
+
+
+</head>
+<body >
+
+    <?php include("view/header.php")?>
+<div class="hero" style="background-image: url('public/images/1.jpg');">
+    <div align="center" class="txtc">
+ <form class="revision">        
+        <h1> Nombre : Diego </h1>
+        <h1> Apellido : Moreles </h1>
+        <h1> Cedula : 8-123-5124 </h1>
+        <h1> Pais : Panama </h1>
+        <h1> Provincia : Panama </h1>
+        <h1> Correo : diegomrales@outlook.com </h1>
+        <h1> telefono : 224-3128 </h1>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" onClick="ComprobarClave()" value="Aceptar" >Confirmar</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" onClick="ComprobarClave()" value="Aceptar" >Editar Informacion</button>
+    </div>
+ </form>  
+</div>
+    <?php include("view/footer.php")?>
+</body>
+</html>
