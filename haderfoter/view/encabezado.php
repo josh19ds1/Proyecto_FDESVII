e995cc5c2c62cdedbf886d377522f074958e1d9a
AntonioXXD <113739002+AntonioXXD@users.noreply.github.com>
2022-10-10 21:21:39 -0500

    10del10 21h

--- haderfoter/view/encabezado.php
+++ haderfoter/view/encabezado.php
@@ -18,7 +18,7 @@
     <!-- Style -->
     <link rel="stylesheet" href="public/css/headerfoder.css">
 
-    <title>Website Menu #4</title>
+    <title>Congreso2024</title>
 </head>
   <body>
 
@@ -120,56 +120,7 @@
 
       </header>
 
-    <div class="hero" style="background-image: url('images/1.jpg');"></div>
+    <div class="hero" style="background-image: url('public/images/1.jpg');"></div>
   
-    <!--foter-->
-    <section class="general formu">
-      <!--parte de la informacion coludna izquierda-->
-      <section class="form">
-          <h2>¿Necesitas ayuda?</h2>
-          <p>llene el formulario para una pronta atencion por parte <br> de nuestro
-              equipo de trabajo para solucionarle el problema <br> de forma rapida y eficiente.</p>
-          <!--comenzo de el cuadro formulario izquierda-->
-          <form action="">
-              <section class="nombre">
-                  <input placeholder="Ej. Jose Gonzalez " type="text">
-              </section>
-              <section class="email">
-                  <input placeholder="Ej. EstebanRamirez@correo.com" type="email">
-              </section>
-              <section class="texto">
-                  <textarea  placeholder="Ej. Mi nombre es Maez Aguilar" rows="4"></textarea>
-              </section>
-              <section>
-                  <input class="gs22 boton2" type="submit" value="Enviar">
-              </section>
-          </form>
-      </section>
-      <section class="datos">
-          <!--Parte derecha redes-->
-          <div>
-              <ul>
-                <li class="Twitter"> <i class="bx bxl-twitter bx-xs"></i> <a href="">Twitter.com/Indefinido</a> </li>
-                <!--Twitter-->
-                <li class="Facebook"> <i class="bx bxl-facebook bx-xs"></i> <a href=""> Facebook.com/Indefinido</a> </li>
-                <!--Facebook-->
-                <li class="Instagram"> <i class="bx bxl-instagram bx-xs"></i> <a href="">Instagram.com/Indefinido</a> </li>
-                <!--Instagram-->
-                <li class="Direccion"> <i class="bx bxl-gmail bx-xs"></i> </a>Information@untitled.com</li>
-                <!--Dirrecion de correo-->
-                <li class="Calle">Campus Victor Levi Sasso,Ancón, Panamá., Vía Centenario, Panamá</li>
-            </ul>
-          </div>
-      </section>
-  </section>
-
-  <hr>
-  <!--footer-->
-  <footer class="derecho">
-      <section>
-          <p>©Derechos Reservados; | | |1LS132</p>
-          <!--dirrecion de el link que de imaguen que tenia-->
-      </section>
-  </footer>
   </body>
 </html>
