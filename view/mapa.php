a6f89712982917a0c4941367db2c8306c1a45879
Bryan Sanchez <84791164+BryanAxelG@users.noreply.github.com>
2022-10-10 21:20:43 -0600

    Add files via upload

---
+++ view/mapa.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Congreso UTP &#8211; 2024</title>
+   <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/sign-in/">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+
+<link href="public/css/bootstrap.min.css" rel="stylesheet">
+
+<link rel="stylesheet"  href="public/css/CongresoStyle.css">
+<style>
+
+section{
+background-image: url("public/imagenes/utp-imagen.jpg");
+background-repeat: no-repeat;
+background-attachment: fixed;
+background-position: center center;
+background-size:cover;
+background-blend-mode: darken;
+height: 100%;}
+
+
+</style>
+</head>
+<body >
+
+<div class="fondoFoto">
+   <header class="headerColor"> 
+     <div class="fondo">  
+       <strong>
+         <p ><h3>BIENVENIDO AL CONGRESO 2024</h3></p>
+      </strong>
+      
+    </div>
+    <div>
+         <img class="logo" src="public/imagenes/LogoUtp.jpg" width="65px" height="65px">  </div> 
+      <div class="titulo">
+       <marquee direction="right" 
+        behavior="alternate" >
+          <div >
+            <a target="_blank" href="https://twitter.com/congresoutp" class="link">
+               <i class='bx bxl-twitter' ></i>
+            </a>
+           <a target="_blank" href="https://www.facebook.com/CongresoUTP/" class="link">
+               <i class="bx bxl-facebook facebook"></i>
+           </a>
+           <a target="_blank" href="https://www.instagram.com/congresoutp/" class="link">
+               <i class="bx bxl-instagram instagram"></i>
+           </a>
+          
+          </div>   
+     </marquee>
+    </div>
+
+
+   </header>
+<br>
+ <div class="div1">
+  <nav class="navbar">
+		<ul class="ulbar">	
+      <li><a href="#">Registro</a></li>
+      <li><a href="#">Administrador</a></li>
+      <li><a href="view/paginaprincipal.php">Cronograma</a></li>
+      <li><a href="view/mapa.php">Mapa</a></li> 
+		</ul>
+	</nav>
+ </div>
+ <section>
+   <br><br>
+
+
+   
+ <h2>MAPA DEL CONGRESO 2024</h2>
+   
+      
+      <br><br>
+</div> 
+</section> 
+<footer>
+   <strong><p >&copy;2022 MASTER DEVELOPER</p></strong>
+<p > Bryan Sanchez, Jose Gonzalez,  Maez Aguilar, Esteban Ramirez, Arundel Fabrega</p>
+
+</footer>
+</body>
+</html>
